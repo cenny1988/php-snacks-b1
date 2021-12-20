a828f7f2cb59c33fb4860730e2fc71c5509fb448
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-12-20 19:30:12 +0100

    es.5 Done

---
+++ snack5/index.php
@@ -0,0 +1,31 @@
+<!-- ## Snack 5
+
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack5</title>
+</head>
+<body>
+    <?php
+        $paragraph = 'Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.';
+    ?>
+
+    <p>
+        <?php echo $paragraph ?>
+    </p>
+
+    <?php
+
+        $paragraphs = explode('.', $paragraph);
+
+        for ($i=0; $i < count($paragraphs); $i++) { 
+            echo '<p>' .  $paragraphs[$i] . '</p>';
+        }
+    ?>
+</body>
+</html>
